fd02640a4e15eab293ba433f0da66531111b145a
Dhvanii <pdhvani2710@gmail.com>
2026-06-08 15:10:39 +0530

    cleared login

---
+++ LOGIN/frontEnd/app.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Login from "./pages/login";
+import Register from "./pages/register";
+
+function App() {
+  const [showRegister, setShowRegister] = useState(false);
+
+  return showRegister ? (
+    <Register onSwitch={() => setShowRegister(false)} />
+  ) : (
+    <>
+      <Login />
+      <div style={{ textAlign: "center", marginTop: 16 }}>
+        <span>Don't have an account? </span>
+        <button
+          type="button"
+          onClick={() => setShowRegister(true)}
+          style={{
+            background: "none",
+            border: "none",
+            color: "blue",
+            textDecoration: "underline",
+            cursor: "pointer",
+          }}
+        >
+          Register
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
